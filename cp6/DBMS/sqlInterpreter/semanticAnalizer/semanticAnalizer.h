2cc0d1a0a19cba9e1617ee41428400bb9349c57f
utyfull <serg_misha1@mail.ru>
2024-04-09 23:29:37 +0300

    rename to sintax

--- cp6/DBMS/sqlInterpreter/semanticAnalizer/semanticAnalizer.h
+++ cp6/DBMS/sqlInterpreter/semanticAnalizer/semanticAnalizer.h
@@ -1,21 +1,4 @@
-#ifndef _SEMANTIC_ANALYZER_H_
-#define _SEMANTIC_ANALYZER_H_
-
-#include "../lexicalAnalizer/lexicalAnalizer.h"
-
-typedef struct ASTNode ASTNode;
-
-typedef struct ASTNode
-{
-    tokenType type;
-    char lexeme[100];
-    ASTNode *children[20];
-    int numChildren;
-} ASTNode;
-
-ASTNode *createASTNode(tokenType type, const char *lexeme);
-void freeAST(ASTNode **root);
-ASTNode *parse(token *tokenList, int numTokens);
-void printASTNode(ASTNode *node, int depth);
+#ifndef _SEMANTIC_ANALIZER_H_
+#define _SEMANTIC_ANALIZER_H_
 
 #endif
---
+++ cp6/DBMS/sqlInterpreter/sintaxAnalizer/sintaxAnalizer.h
@@ -0,0 +1,21 @@
+#ifndef _SEMANTIC_ANALYZER_H_
+#define _SEMANTIC_ANALYZER_H_
+
+#include "../lexicalAnalizer/lexicalAnalizer.h"
+
+typedef struct ASTNode ASTNode;
+
+typedef struct ASTNode
+{
+    tokenType type;
+    char lexeme[100];
+    ASTNode *children[20];
+    int numChildren;
+} ASTNode;
+
+ASTNode *createASTNode(tokenType type, const char *lexeme);
+void freeAST(ASTNode **root);
+ASTNode *parse(token *tokenList, int numTokens);
+void printASTNode(ASTNode *node, int depth);
+
+#endif
