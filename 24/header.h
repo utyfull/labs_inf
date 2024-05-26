829d133e5eb79b5e8c63aecc6355c5e4468147df
utyfull <serg_misha1@mail.ru>
2024-05-26 09:57:43 +0300

    lab24

--- 24/header.h
+++ 24/header.h
@@ -3,13 +3,17 @@
 
 typedef struct Node
 {
-    char data;
+    char data[100];
     struct Node *left;
     struct Node *right;
 } Node;
 
-Node *createNode(char data);
-Node *buildTree(char *str, int *index);
+Node *createNode(char *data);
+Node *buildTree(char *str, char *sep);
 void printTree(Node *root);
+void freeRoot(Node *root);
+void makeSumSquare(Node *root);
+void checkOperators(Node *root);
+int toSqrt(int num);
 
 #endif
--- 24/lab24.c
+++ 24/lab24.c
@@ -3,58 +3,88 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-Node *createNode(char data)
+Node *createNode(char *data)
 {
     Node *newNode = (Node *)malloc(sizeof(Node));
-    newNode->data = data;
+    strcpy(newNode->data, data);
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
 }
 
-Node *buildTree(char *str, int *index)
+int toSqrt(int num)
 {
-    if (str[*index] == '\0' || str[*index] == ')')
+    double squareRoot = sqrt((double)num);
+    if ((int)squareRoot == squareRoot)
+    {
+        return (int)squareRoot;
+    }
+    else
+    {
+        fprintf(stderr, "Cant extract square!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void makeSumSquare(Node *root)
+{
+    checkOperators(root);
+    int a = toSqrt(atoi(root->left->data));
+    int b = toSqrt(atoi(root->right->right->data));
+    int twOab = (int)(atoi(root->right->left->data) / 2);
+
+    if (a * b == twOab)
+    {
+        printf("(%d + %d)^2\n", a, b);
+    }
+    else
+    {
+        fprintf(stderr, "Can't create square!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void checkOperators(Node *root)
+{
+    if (strcmp(root->data, "+") != 0 || strcmp(root->right->data, "+") != 0)
+    {
+        fprintf(stderr, "Wrong operator while creating sum square!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+Node *buildTree(char *str, char *sep)
+{
+    if (str == NULL || *str == ')')
     {
         return NULL;
     }
 
-    if (isdigit(str[*index]))
+    if (1)
     {
-        Node *root = createNode(str[*index]);
-        (*index)++;
-        return root;
-    }
-    else if (str[*index] == '(')
-    {
-        // Пропускаем открывающую скобку
-        (*index)++;
-
-        Node *root = buildTree(str, index);
-
-        // Следующий символ после рекурсивного вызова должен быть операцией
-        root->data = str[*index];
-        (*index)++;
-
-        root->left = buildTree(str, index);
-        root->right = buildTree(str, index);
-
-        // Следующий символ после правого узла должен быть закрывающей скобкой
-        if (str[*index] == ')')
+        Node *root = NULL;
+        char temp[100];
+        strcpy(temp, str);
+        if (strcmp(temp, "-") == 0 || strcmp(temp, "+") == 0)
         {
-            (*index)++;
+            fprintf(stderr, "Wrong operator position!\n");
+            exit(EXIT_FAILURE);
         }
+        str = strtok(NULL, sep);
 
-        return root;
-    }
-    else
-    {
-        // Операция
-        Node *root = createNode(str[*index]);
-        (*index)++;
-        root->left = buildTree(str, index);
-        root->right = buildTree(str, index);
+        if (str != NULL)
+        {
+            root = createNode(str);
+            root->left = createNode(temp);
+            str = strtok(NULL, sep);
+            root->right = buildTree(str, sep);
+        }
+        else
+        {
+            root = createNode(temp);
+        }
         return root;
     }
 }
@@ -66,21 +96,46 @@ void printTree(Node *root)
         return;
     }
 
-    printf("%c \n", root->data);
+    printf("%s \n", root->data);
     printTree(root->left);
     printTree(root->right);
 }
 
+void freeRoot(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    freeRoot(root->left);
+    freeRoot(root->right);
+    free(root->data);
+}
+
 int main()
 {
-    char str[] = "a + b + c - d";
-    int index = 0;
-    // Node *root = NULL;
 
-    Node *root = buildTree(str, &index);
+    // char str[] = "4 + 8 + 4";
+    char str[100];
+    scanf("%99[^\n]", str);
+
+    if (strcmp(str, "") == 0)
+    {
+        fprintf(stderr, "Need five arguments, 0 given!\n");
+        exit(EXIT_FAILURE);
+    }
+    char *str_copy = strdup(str);
+    char *newstr = strtok(str_copy, " ");
+
+    Node *root = buildTree(newstr, " ");
 
     printf("Binary Tree representation of the string: \n");
     printTree(root);
+
+    makeSumSquare(root);
+
+    free(str_copy);
+    freeRoot(root);
 
     return 0;
 }
